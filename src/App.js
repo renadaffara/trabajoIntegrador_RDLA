eca2f463228139b953557c3f66932df03464f38f
laraantonena <lantonenaposse@udesa.edu.ar>
2026-04-01 17:43:54 -0300

    Fix errores

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
 import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+
         <Switch>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/detail/:id" component={Detail} />
-        <Route path="/favourites" component={Favourite} />
-        <Route path="/login" component={Login} />
-        <Route path="/SearchResults" component={SearchResults} />
-        <Route path="/movies" component={Movies} />
-        <Route path="/series" component={Series} />
-        <Route path="/*" component={NotFound} />
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/detail/:id" component={Detail} />
+          <Route path="/favourites" component={Favourite} />
+          <Route path="/login" component={Login} />
+          <Route path="/SearchResults" component={SearchResults} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/series" component={Series} />
+          <Route path="/*" component={NotFound} />
         </Switch>
+
       <Footer/>
     </div>
   );
