e212268db8a8182b6946236b59bb55f388986505
renadaffara <rdaffara@udesa.edu.ar>
2026-04-01 17:26:19 -0300

    hice app.js conectando los links de las screens

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+        <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/detail/:id" component={Detail} />
+        <Route path="/favourites" component={Favourite} />
+        <Route path="/login" component={Login} />
+        <Route path="/SearchResults" component={SearchResults} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/series" component={Series} />
+        <Route path="/*" component={NotFound} />
+        </Switch>
+      <Footer/>
     </div>
   );
 }
