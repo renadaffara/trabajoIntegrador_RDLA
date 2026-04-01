3088a37b88ed01aa8ba88c81c5514738a16c4a31
renadaffara <rdaffara@udesa.edu.ar>
2026-04-01 17:44:49 -0300

    arreglos y footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'; 
+import Footer from './componentes/Footer/Footer';
 
 function App() {
   return (
---
+++ src/componentes/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+
+function Footer(){
+    return(
+        <p className="Footer"> Renata Daffara | Lara antonena </p>
+    )
+}
+
+export default Footer;
