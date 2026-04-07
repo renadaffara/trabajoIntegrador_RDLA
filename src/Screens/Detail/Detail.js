d7093cdf10e5baaeab7423499cd2390af16adbbe
laraantonena <lantonenaposse@udesa.edu.ar>
2026-04-06 21:03:51 -0300

    edite card

--- src/Screens/Detail/Detail.js
+++ src/Screens/Detail/Detail.js
@@ -18,21 +18,23 @@ class Detail extends Component {
   }
 
   render() {
-    if (!this.state.peli) {
-      return <h2>Cargando...</h2>;
-    }
-
     return (
-      <section>
-        <img
-          src={`https://image.tmdb.org/t/p/w342/${this.state.peli.poster_path}`}
-          alt={this.state.peli.title}
-        />
-        <h1>{this.state.peli.title}</h1>
-        <p>{this.state.peli.overview}</p>
-        <p>Fecha: {this.state.peli.release_date}</p>
-        <p>Rating: {this.state.peli.vote_average}</p>
-      </section>
+      <>
+        {this.state.peli === null ? (
+          <h2>Cargando...</h2>
+        ) : (
+          <section>
+            <img
+              src={`https://image.tmdb.org/t/p/w342/${this.state.peli.poster_path}`}
+              alt={this.state.peli.title}
+            />
+            <h1>{this.state.peli.title}</h1>
+            <p>{this.state.peli.overview}</p>
+            <p>Fecha: {this.state.peli.release_date}</p>
+            <p>Rating: {this.state.peli.vote_average}</p>
+          </section>
+        )}
+      </>
     );
   }
 }
--- src/componentes/Card/Card.js
+++ src/componentes/Card/Card.js
@@ -10,23 +10,9 @@ class Card extends Component {
   }
 
   render() {
-    
-
     return (
-       <article class="single-card-movie">
-                <img src="https://image.tmdb.org/t/p/w500/ombsmhYUqR4qqOLOxAyr5V8hbyv.jpg" class="card-img-top"
-                    alt="...">
-                <div class="cardBody">
-                    <h5 class="card-title">Superman</h5>
-                    <p class="card-text">Superman, a journalist in Metropolis, embarks on a journey to reconcile his
-                        Kryptonian heritage with his human upbringing as Clark Kent.</p>
-                    <a href="movie.html" class="btn btn-primary">Ver más</a>
-                    <a href="" class="btn alert-primary">♥️</a>
-                </div>
-            </article>
-    );
+    )
   }
 }
-
 export default Card;
 
